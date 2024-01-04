8e6fe0d31ed1ea76cf612bfc458e95e74684eb3e
Viktor Revkov <94319033+Goldwik@users.noreply.github.com>
2024-01-04 09:36:15 -0500

    Update raspi-config_install.sh

--- raspi-config_install.sh
+++ raspi-config_install.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
+
 # Check if root
 if [ "$(whoami)" != "root" ]; then
-  whiptail --msgbox "Sorry you are not root. You must type: sudo sh install.sh" $WT_HEIGHT $WT_WIDTH
+  echo "Sorry, you are not root. You must type: sudo sh install.sh"
   exit
 fi
 
-# Check if raspi-config is installed
-if [ $(dpkg-query -W -f='${Status}' raspi-config 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-  whiptail --msgbox "Raspi-config is already installed, try upgrading it within raspi-config..." 10 60
-else
-  wget https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20200601_all.deb -P /tmp
-  apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils -y
-  # Auto install dependancies on eg. ubuntu server on RPI
-  apt-get install -fy
-  dpkg -i /tmp/raspi-config_20200601_all.deb
-  whiptail --msgbox "Raspi-config is now installed, run it by typing: sudo raspi-config" 10 60
+# Check if Raspberry Pi repository is already added
+if ! grep -q "deb http://archive.raspberrypi.org/debian/ buster main" /etc/apt/sources.list; then
+  echo "deb http://archive.raspberrypi.org/debian/ buster main" >> /etc/apt/sources.list
 fi
 
-exit
+# Check if Raspberry Pi Foundation public key is already imported
+if ! apt-key list | grep -q "7FA3303E"; then
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7FA3303E
+fi
+
+# Update package list
+apt update
+
+# Check if raspi-config is already installed
+if [ $(dpkg-query -W -f='${Status}' raspi-config 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
+  echo "Raspi-config is already installed, try upgrading it within raspi-config..."
+else
+  apt install -y sudo libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils raspi-config
+  echo "Raspi-config is now installed. You can run it by typing: sudo raspi-config"
+fi
